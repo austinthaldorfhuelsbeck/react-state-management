bd5f80b0d71d25197dd8559b68014e8bbbb05f8c
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-02-02 23:31:15 -0800

    Learning async state

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { useState } from "react";
 
 function App() {
   const [subscribed, setSubscribed] = useState(false);
+  const [subscribedCount, setSubscribedCount] = useState(0);
+  const [alerts, setAlerts] = useState(false);
+
   return (
     <section>
-      <p>Click subscribe to stay updated!</p>
-      <button onClick={() => setSubscribed(!subscribed)}>
+      <p>Please click to subscribe to my updates!</p>
+      <p>Subscriber Count: {subscribedCount}</p>
+      <button
+        onClick={() => {
+          setSubscribed(!subscribed);
+          setSubscribedCount(subscribedCount + 1);
+          if (!alerts) setAlerts(true);
+        }}
+      >
         {subscribed ? "Unsubscribe" : "Subscribe"}
+      </button>
+      <button onClick={() => setAlerts(!alerts)}>
+        {alerts ? "Stop Email Alerts" : "Get Email Alerts"}
       </button>
     </section>
   );
